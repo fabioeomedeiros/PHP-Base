21ce8b02d138043c8c9477d145fe2079ee72230c
Fabio Medeiros <fabioeomedeiros@gmail.com>
2021-09-08 01:59:24 -0300

    add

--- 004_Formatacao_de_texto/arquivo.php
+++ 004_Formatacao_de_texto/arquivo.php
@@ -7,14 +7,28 @@
     <link rel="shortcut icon" type="image/x-icon" href="../imagens/favicon.ico">
     <link rel="stylesheet" type="text/css" href="../css/estilo.css">
     <!-- <script type="text/javascript" src="script.js"></script> -->
-    <title>Título</title>
+
+    <?php
+        $txt = isset($_GET["nm-texto"])?$_GET["nm-texto"]:"[texto não informado]";
+        $tam = isset($_GET["nm-tam"])?$_GET["nm-tam"]:"14pt";
+        $cor = isset($_GET["nm-cor"])?$_GET["nm-cor"]:"#ff0000";
+    ?>
+
+    <style>
+        span#texto {
+            font-size: <?php echo $tam; ?>;
+            color: <?php echo $cor; ?>;
+        }
+    </style>
+
+    <title>Formatação de Texto</title>
 </head>
 <body>
     <div id="container">
-        <h1>Título</h1>
+        <h1>Formatação de Texto</h1>
         <div id="code">
             <?php
-
+                echo "<span id='texto'>$txt</span>";
             ?>
         </div>
         <a class="back" href="javascript:history.back()">Voltar</a>
