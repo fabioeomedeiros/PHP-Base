9f77bf7b3449c1db08b8e122b46597fc8ad8f8a1
Fabio Medeiros <fabioeomedeiros@gmail.com>
2021-09-14 19:33:19 -0300

    add

--- 000_Matriz/arquivo.php
+++ 000_Matriz/arquivo.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"> -->
-    <link rel="stylesheet" type="text/css" href="estilo.css">
+    <link rel="shortcut icon" type="image/x-icon" href="../imagens/favicon.ico">
+    <link rel="stylesheet" type="text/css" href="../css/estilo.css">
     <!-- <script type="text/javascript" src="script.js"></script> -->
     <title>Título</title>
 </head>
 <body>
     <div id="container">
         <h1>Título</h1>
-        <div id="code">
+        <div id="display">
             <?php
 
             ?>
