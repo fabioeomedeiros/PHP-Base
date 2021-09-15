1c3955690307ac9901a96d25871534bf2ab83136
Fabio Medeiros <fabioeomedeiros@gmail.com>
2021-09-15 11:00:36 -0300

    add

--- 010_For/arquivo.php
+++ 010_For/arquivo.php
@@ -5,14 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/x-icon" href="../imagens/favicon.ico">
-    <link rel="stylesheet" type="text/css" href="../css/estilo2.css">
+    <link rel="stylesheet" type="text/css" href="estilo.css">
     <!-- <script type="text/javascript" src="script.js"></script> -->
-
-    <!-- <style>
-        td {
-            padding:0px 5px;
-        }
-    </style> -->
 
     <title>For</title>
 </head>
